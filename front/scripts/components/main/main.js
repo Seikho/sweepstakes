355bd19312edb57e993b926703ed7553bdc22bb8
Carl <carl@carlossus.com>
2015-09-19 18:07:18 +0800

    Implement basic Main viewmodel

---
+++ front/scripts/components/main/main.js
@@ -0,0 +1,28 @@
+var ko = require('knockout');
+var FB = require('facebook');
+var MainViewModel = (function () {
+    function MainViewModel() {
+        var _this = this;
+        this.currentUser = ko.observable('');
+        this.loginButtonText = ko.computed(function () {
+            var user = _this.currentUser() || '';
+            return user.length > 0 ? user : 'Login';
+        });
+        this.checkCurrentUser();
+    }
+    MainViewModel.prototype.loginButton = function () {
+        if (this.loginButtonText() === 'Login')
+            return;
+        FB.login(this.setCurrentUser);
+    };
+    MainViewModel.prototype.checkCurrentUser = function () {
+        FB.getLoginStatus(this.setCurrentUser);
+    };
+    MainViewModel.prototype.setCurrentUser = function (status) {
+        if (status.status !== 'connected')
+            return;
+        FB.api('/me', function (info) { return console.log(info); });
+    };
+    return MainViewModel;
+})();
+//# sourceMappingURL=main.js.map
---
+++ front/scripts/components/main/main.ts
@@ -0,0 +1,29 @@
+import ko = require('knockout');
+import FB = require('facebook');
+
+class MainViewModel {
+    constructor(){
+        this.checkCurrentUser();
+    }
+    
+    currentUser = ko.observable('');
+    loginButtonText = ko.computed(() => {
+        var user = this.currentUser() || '';
+        
+        return user.length > 0 ? user : 'Login';
+    });
+    
+    loginButton() {
+        if (this.loginButtonText() === 'Login') return;
+        FB.login(this.setCurrentUser);
+    }
+    
+    checkCurrentUser() {
+        FB.getLoginStatus(this.setCurrentUser);
+    }
+    
+    setCurrentUser(status: FB.Status) {
+        if (status.status !== 'connected') return;
+        FB.api('/me', info => console.log(info));
+    }
+}
