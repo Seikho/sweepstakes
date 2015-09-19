6a7d068338da241e00aeb6b4abf1e8a234cafcf3
Carl <carl@carlossus.com>
2015-09-19 18:45:22 +0800

    Add db location module to repo

---
+++ src/store/location.js
@@ -0,0 +1,4 @@
+var path = require('path');
+exports.main = path.resolve('../../sweepstakes.db');
+exports.base = path.resolve('../../sweepstakes.base.db');
+//# sourceMappingURL=location.js.map
---
+++ src/store/location.ts
@@ -0,0 +1,4 @@
+import path = require('path');
+
+export var main = path.resolve('../../sweepstakes.db');
+export var base = path.resolve('../../sweepstakes.base.db');
