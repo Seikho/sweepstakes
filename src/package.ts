2e7c6b080b94120edbbb4f5e2b84b2ae2d5fd3c0
Carl <carl@carlossus.com>
2015-09-19 13:16:09 +0800

    Add PackageJson interface to package export

--- src/package.ts
+++ src/package.ts
@@ -3,4 +3,14 @@ import path = require('path');
 export = packageJson;
 
 var packagePath = path.resolve(path.join('..', 'package.json'));
-var packageJson = JSON.parse(fs.readFileSync(packagePath).toString());
+var packageJson: PackageJson = JSON.parse(fs.readFileSync(packagePath).toString());
+
+interface PackageJson {
+    name: string,
+    version: string,
+    description: string,
+    main: string,
+    author: string,
+    license: string,
+    homepage: string,
+};
