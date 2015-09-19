38a986339558ea32df066e7c58eace55fb299d0a
Carl <carl@carlossus.com>
2015-09-19 17:49:06 +0800

    Add facebook API functions to type defs

--- src/typings/sweepstakes.d.ts
+++ src/typings/sweepstakes.d.ts
@@ -2,3 +2,31 @@ interface NodeRequireFunction {
     (modules: string[], callback?: (...args) => any);
     config(options: any);
 }
+
+declare namespace FB {
+    function getLoginStatus(callback: LoginCallback): void;
+    function init(options: Options): void;
+    function login(callback: LoginCallback): void;
+    
+    interface LoginCallback {
+        (status: Status) => void;
+    }
+    
+    interface Options {
+        appId: string;
+        version: string;
+        xfbml?: boolean;
+        [option: string]: any;
+    }
+    
+    interface Status {
+        status: string;
+        authResponse: {
+            accessToken: string;
+            expiresIn: string;
+            signedRequest: string;
+            userID: string;
+        }
+    }
+}
+
