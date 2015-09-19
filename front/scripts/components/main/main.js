49f6a11679a087a783343a82544a0effe69a1ec7
Carl <carl@carlossus.com>
2015-09-19 18:31:02 +0800

    Complete basic login functionality

--- front/scripts/components/main/main.js
+++ front/scripts/components/main/main.js
@@ -8,21 +8,25 @@ var MainViewModel = (function () {
             var user = _this.currentUser() || '';
             return user.length > 0 ? user : 'Login';
         });
+        this.loginButton = function () {
+            if (_this.loginButtonText() !== 'Login')
+                return;
+            FB.login(_this.setCurrentUser);
+        };
+        this.checkCurrentUser = function () {
+            FB.getLoginStatus(_this.setCurrentUser);
+        };
+        this.setCurrentUser = function (status) {
+            if (status.status !== 'connected')
+                return;
+            FB.api('/me', function (info) {
+                if (!info.name)
+                    return;
+                _this.currentUser(info.name);
+            });
+        };
         this.checkCurrentUser();
     }
-    MainViewModel.prototype.loginButton = function () {
-        if (this.loginButtonText() === 'Login')
-            return;
-        FB.login(this.setCurrentUser);
-    };
-    MainViewModel.prototype.checkCurrentUser = function () {
-        FB.getLoginStatus(this.setCurrentUser);
-    };
-    MainViewModel.prototype.setCurrentUser = function (status) {
-        if (status.status !== 'connected')
-            return;
-        FB.api('/me', function (info) { return console.log(info); });
-    };
     return MainViewModel;
 })();
 module.exports = MainViewModel;
--- front/scripts/components/main/main.ts
+++ front/scripts/components/main/main.ts
@@ -9,22 +9,24 @@ class MainViewModel {
     
     currentUser = ko.observable('');
     loginButtonText = ko.computed(() => {
-        var user = this.currentUser() || '';
-        
+        var user = this.currentUser() || '';        
         return user.length > 0 ? user : 'Login';
     });
     
-    loginButton() {
-        if (this.loginButtonText() === 'Login') return;
+    loginButton = () => {
+        if (this.loginButtonText() !== 'Login') return;
         FB.login(this.setCurrentUser);
     }
     
-    checkCurrentUser() {
+    checkCurrentUser = () => {
         FB.getLoginStatus(this.setCurrentUser);
     }
     
-    setCurrentUser(status: FB.Status) {
+    setCurrentUser = (status: FB.Status) => {
         if (status.status !== 'connected') return;
-        FB.api('/me', info => console.log(info));
+        FB.api('/me', info => {
+            if (!info.name) return;
+            this.currentUser(info.name);
+        });
     }
 }
