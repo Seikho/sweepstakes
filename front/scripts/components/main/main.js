58e41fac03f3af345c18f40ce87230a52f13c32d
Carl <carl@carlossus.com>
2015-09-20 21:57:51 +0800

    Add front-end viewmodel functionality Implement: - Authentication - Get groups - Get sweepstakes

--- front/scripts/components/main/main.js
+++ front/scripts/components/main/main.js
@@ -1,5 +1,6 @@
 var ko = require('knockout');
 var FB = require('facebook');
+var $ = require('jquery');
 var MainViewModel = (function () {
     function MainViewModel() {
         var _this = this;
@@ -24,6 +25,29 @@ var MainViewModel = (function () {
                     return;
                 _this.currentUser(info.name);
             });
+            _this.login(status);
+        };
+        this.groups = ko.observableArray([]);
+        this.sweepstakes = ko.observableArray([]);
+        this.createGroup = function () { };
+        this.createSweepstake = function () { };
+        this.findUsers = function (search) { };
+        this.addEntry = function () { };
+        this.login = function (status) {
+            $.post('/login', status)
+                .then(function () {
+                _this.getGroups(FB.getAccessToken());
+                _this.getSweepstakes(FB.getAccessToken());
+            })
+                .fail(function (err) { return console.log(err); });
+        };
+        this.getGroups = function (token) {
+            $.get('/groups', { token: token })
+                .then(_this.groups);
+        };
+        this.getSweepstakes = function (token) {
+            $.get('/sweepstakes', { token: token })
+                .then(_this.sweepstakes);
         };
         this.checkCurrentUser();
     }
--- front/scripts/components/main/main.ts
+++ front/scripts/components/main/main.ts
@@ -1,32 +1,61 @@
 import ko = require('knockout');
 import FB = require('facebook');
+import $ = require('jquery');
 export = MainViewModel;
 
 class MainViewModel {
-    constructor(){
+    constructor() {
         this.checkCurrentUser();
     }
-    
+
     currentUser = ko.observable('');
     loginButtonText = ko.computed(() => {
-        var user = this.currentUser() || '';        
+        var user = this.currentUser() || '';
         return user.length > 0 ? user : 'Login';
     });
-    
+
     loginButton = () => {
         if (this.loginButtonText() !== 'Login') return;
         FB.login(this.setCurrentUser);
     }
-    
+
     checkCurrentUser = () => {
         FB.getLoginStatus(this.setCurrentUser);
     }
-    
+
     setCurrentUser = (status: FB.Status) => {
         if (status.status !== 'connected') return;
         FB.api('/me', info => {
             if (!info.name) return;
             this.currentUser(info.name);
         });
+        this.login(status);
+    }
+
+    groups = ko.observableArray<Sweepstakes.Group>([]);
+    sweepstakes = ko.observableArray<Sweepstakes.Sweepstake>([]);
+
+    createGroup = () => { }
+    createSweepstake = () => { }
+    findUsers = (search: string) => { }
+    addEntry = () => { }
+
+    login = (status: FB.Status) => {
+        $.post('/login', status)
+            .then(() => {
+                this.getGroups(FB.getAccessToken());
+                this.getSweepstakes(FB.getAccessToken());
+            })
+            .fail(err => console.log(err));
+    }
+
+    getGroups = (token: string) => {
+        $.get('/groups', { token })
+            .then(this.groups);
+    }
+
+    getSweepstakes = (token: string) => {
+        $.get('/sweepstakes', { token })
+            .then(this.sweepstakes);
     }
 }
