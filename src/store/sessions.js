29fba1a13fb92d5f31bdb25a329c79bb68a3df74
Carl <carl@carlossus.com>
2015-09-20 21:05:53 +0800

    Implement sessions api

--- src/store/sessions.js
+++ src/store/sessions.js
@@ -1,20 +1,55 @@
 var db = require('./db');
 var users = require('./users');
+var tokenStatus = require('../api/facebook/tokenStatus');
+function isValid(token) {
+    return getByToken(token)
+        .then(function (session) { return !!session; });
+}
+exports.isValid = isValid;
 function getByToken(token) {
+    var authToken;
+    if (typeof token === 'string')
+        authToken = token;
+    if (typeof token !== 'string')
+        authToken = token.authResponse.accessToken;
     return db('sessions')
         .select()
-        .where({ token: token.authResponse.accessToken })
+        .where({ token: authToken })
         .innerJoin('users', 'users.id', 'sessions.userId')
         .then(function (responses) { return responses[0]; });
 }
 exports.getByToken = getByToken;
 function create(token) {
-    users.getByFacebookId(token.authResponse.userID);
+    return getByToken(token)
+        .then(function (session) {
+        if (!!session)
+            return Promise.resolve(true);
+        return createHandler(token);
+    });
 }
 exports.create = create;
-function createHandler(user) {
-    if (user)
-        return Promise.resolve(user.id);
-    return users.create(user.facebookId);
+function createHandler(token) {
+    return tokenStatus(token)
+        .then(function (status) {
+        if (status.error)
+            return Promise.reject('Unable to verify token: ' + status.error.message);
+        return createSession(token);
+    }).catch(function (err) { return Promise.reject(err); });
+}
+function createSession(token) {
+    var storeSession = function (id) { return db('sessions')
+        .insert({
+        userId: id,
+        token: token.authResponse.accessToken
+    })
+        .then(function () { return true; })
+        .catch(function () { return false; }); };
+    var fbId = token.authResponse.userID;
+    return users.get(fbId)
+        .then(function (user) {
+        if (!user)
+            return users.create(fbId).then(storeSession);
+        return storeSession(user.id);
+    });
 }
 //# sourceMappingURL=sessions.js.map
--- src/store/sessions.ts
+++ src/store/sessions.ts
@@ -1,21 +1,57 @@
 import db = require('./db');
 import users = require('./users');
+import tokenStatus = require('../api/facebook/tokenStatus');
 
-export function getByToken(token: FB.Status): Promise<Sweepstakes.UserSession> {
+export function isValid(token: FB.Status|string) {
+    return getByToken(token)
+        .then(session => !!session);
+}
+
+export function getByToken(token: FB.Status|string): Promise<Sweepstakes.UserSession> {
+    var authToken: string;
+    if (typeof token === 'string') authToken = token;
+    if (typeof token !== 'string') authToken = token.authResponse.accessToken;
+    
     return db('sessions')
         .select()
-        .where({ token: token.authResponse.accessToken })
+        .where({ token: authToken })
         .innerJoin('users', 'users.id', 'sessions.userId')
         .then(responses => responses[0]);
 }
 
 export function create(token: FB.Status) {
-    users.getByFacebookId(token.authResponse.userID)
-        
+    return getByToken(token)
+        .then(session => {
+            if (!!session) return Promise.resolve(true);
+            return createHandler(token);
+        });
 }
 
-function createHandler(user?: Sweepstakes.User) {
-    if (user) return Promise.resolve(user.id);
-    
-    return users.create(user.facebookId)
+function createHandler(token: FB.Status) {
+    return tokenStatus(token)
+        .then(status => {
+            if (status.error)
+                return Promise.reject('Unable to verify token: ' + status.error.message);
+
+            return createSession(token);
+        }).catch(err => Promise.reject(err));
+
+}
+
+function createSession(token: FB.Status) {
+    var storeSession = id => db('sessions')
+        .insert({
+            userId: id,
+            token: token.authResponse.accessToken
+        })
+        .then(() => true)
+        .catch(() => false);
+
+    var fbId = token.authResponse.userID;
+
+    return users.get(fbId)
+        .then(user => {
+            if (!user) return users.create(fbId).then(storeSession)
+            return storeSession(user.id)
+        });
 }
